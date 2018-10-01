b58b6b63869f844e68292d140cff9310bbc48042
brrathod <rathod.bhavik@gmail.com>
2018-10-02 00:51:01 +0100

    Update HelloWorldController.cs
    updated namespace to app

--- app/Controllers/HelloWorldController.cs
+++ app/Controllers/HelloWorldController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace app.Controllers
 {
     public class HelloWorldController : Controller
     {
